36256fa1fb0690fb6ae8278270999c169dddedd5
Jonathan May <jmay4826@gmail.com>
2019-08-22 08:10:49 -0500

    Revert "Add lecture code"
    This reverts commit 80e5f15f25274c42aae3b46ba31b2eaf99eab2bb.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,26 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(){
-    super();
-    // we don't have any characters at first, so we need an empty array as a placeholder
-    this.state = {
-      characters: []
-    }
-  }
-  /* Use componentDidMount if you want the data to be requested immediately when the user lands on the page
-   componentDidMount(){ 
-     // You can make any kind of HTTP request with axios by changing it from
-     // axios.post to axios.get or axios.put or axios.delete
-
-     // If you need to send data with your request, send it as an object as the second argument to the axios function
-
-     let promise = axios.post("https://www.breakingbadapi.com/api/characters", {cheeseburger: "welldone"})
-  
-     promise.then(response => {
-       console.log(response.data);
-       this.setState({characters: response.data})
-     })
-   }
-   */
-  render(){
-
-    let characterList = this.state.characters.map(character => {
-      return <div>
-      <h1>{character.name}</h1>
-      {/* you probably shouldn't do inline styling like this */}
-      <img style={{maxWidth: '100px'}} src={character.img} />
-      </div>
-    })
-  
+function App() {
   return (
     <div className="App">
-    {/* Or you can request the information when the user clicks a button  */}
-<button onClick={() => {
-  let promise = axios.get("https://www.breakingbadapi.com/api/characters")
-    promise.then(response => {
-      console.log(response.data);
-      this.setState({characters: response.data})
-    })
-}}>Get Information</button>
-      {characterList}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-  }
 }
 
 export default App;
